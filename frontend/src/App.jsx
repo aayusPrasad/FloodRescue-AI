6e2612da01e4a0073ac35fc36a73e6b53280313f
Aayush_Prasad <166906326+aayusPrasad@users.noreply.github.com>
2026-05-20 20:37:30 +0530

    Refactor routing to include authentication and new pages
    Added Login and Signup pages with protected routes for Analyze, Dashboard, Drone Survey, Flood Map, and Impact 3D pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,31 @@ import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import AnalyzePage from './pages/AnalyzePage'
-import DroneSurveyPage from './pages/DroneSurveyPage'
 import DashboardPage from './pages/DashboardPage'
-import FloodMapPage from './pages/FloodMapPage'
-import FloodImpact3DPage from './pages/FloodImpact3DPage'
 import AboutPage from './pages/AboutPage'
 import SafetyTipsPage from './pages/SafetyTipsPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import DroneSurveyPage from './pages/DroneSurveyPage'
+import FloodMapPage from './pages/FloodMapPage'
+import Impact3DPage from './pages/FloodImpact3DPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/analyze" element={<AnalyzePage />} />
-        <Route path="/drone-survey" element={<DroneSurveyPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/flood-map" element={<FloodMapPage />} />
-        <Route path="/impact-3d" element={<FloodImpact3DPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/safety-tips" element={<SafetyTipsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        <Route path="/analyze" element={<ProtectedRoute><AnalyzePage /></ProtectedRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+        <Route path="/drone-survey" element={<ProtectedRoute><DroneSurveyPage /></ProtectedRoute>} />
+        <Route path="/flood-map" element={<ProtectedRoute><FloodMapPage /></ProtectedRoute>} />
+        <Route path="/impact-3d" element={<ProtectedRoute><Impact3DPage /></ProtectedRoute>} />
       </Route>
     </Routes>
   )
